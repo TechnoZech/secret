69dc132c8cfdd745134ac241225a1f6886f3feb6
unknown <sharmadharmesh1998@gmail.com>
2022-10-25 14:27:53 +0530

    Hash security

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const app = express();
+const md5 = require("md5");
 
 app.use(express.static("public"));
 app.set("view engine", 'ejs');
@@ -16,11 +16,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +36,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -56,7 +51,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
